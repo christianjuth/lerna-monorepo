baa33c438c57cbbf8da13eaa7679d4744e09536a
Christian Juth <cjuth2@gmail.com>
2021-11-23 22:39:00 -0500

    refactor: move benchmark code to seprate file

--- packages/tictactoe-engine/src/tictactoe-engine.ts
+++ packages/tictactoe-engine/src/tictactoe-engine.ts
@@ -179,28 +179,3 @@ function convertGameStateToNuralNetworkData(
   const cells = gameState.map(getIntRepresentationOfCell);
   return [getIntRepresentationOfCell(player), ...cells];
 }
-
-export async function checkPerformance(method = "miniMax") {
-  const { performance } = await require("perf_hooks");
-  let board = ["", "", "", "", "", "", "", "", ""];
-  getBestMoveWasmNuralNetwork(board);
-
-  const runtimes: number[] = [];
-
-  while (checkWinner(board) === undefined) {
-    const t1 = performance.now();
-    if (method === "nuralNetwork") {
-      board = getBestMoveNuralNetwork(board) ?? [];
-    } else if (method === "wasmNuralNetwork") {
-      board = (await getBestMoveWasmNuralNetwork(board)) ?? [];
-    } else {
-      board = getBestMovesMiniMax(board) ?? [];
-    }
-    const t2 = performance.now();
-    runtimes.push(t2 - t1);
-  }
-
-  const perf = runtimes.reduce((a, b) => a + b) / runtimes.length;
-  console.log(perf);
-  return perf;
-}
