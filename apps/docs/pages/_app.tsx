1b959968dc75405560ed27ff8d5855cec19b9c89
Christian Juth <cjuth2@gmail.com>
2021-12-17 01:51:27 -0500

    fix: docs zoom in

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -3,25 +3,32 @@ import { Theme } from '@christianjuth/ui'
 import { AuthProvier } from '../components/Gun'
 import { Navbar } from '../components/Navbar'
 import { use100vh } from 'react-div-100vh'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const pageHeight = use100vh() ?? 0
 
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+      </Head>
       <Theme
         base={{
-          primary: ({ l }) => [0,0,l,0],
-          accent1: ({ l }) => [0,0,l,0],
-          gray: ({ l }) => [0,0,l,0],
+          primary: ({ l }) => [0, 0, l, 0],
+          accent1: ({ l }) => [0, 0, l, 0],
+          gray: ({ l }) => [0, 0, l, 0],
         }}
         dark={{
-          gray: ({ l }) => [0,0,100-l,0],
+          gray: ({ l }) => [0, 0, 100 - l, 0],
         }}
       />
       <AuthProvier>
-        <div style={{minHeight: pageHeight, display: 'flex', flexDirection: 'column'}}>
-          <Navbar/>
+        <div style={{ minHeight: pageHeight, display: 'flex', flexDirection: 'column' }}>
+          <Navbar />
           <Component {...pageProps} />
         </div>
       </AuthProvier>
