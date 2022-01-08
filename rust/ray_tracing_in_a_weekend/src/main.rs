6fe3ed19cb4be3d7a6b5ebce1f6cccd544ea73cb
Christian Juth <cjuth2@gmail.com>
2022-01-07 22:45:30 -0500

    feat: start rust ray tracing

---
+++ rust/ray_tracing_in_a_weekend/src/main.rs
@@ -0,0 +1,44 @@
+use rand::Rng;
+use std::io::Write;
+use std::fs::OpenOptions;
+
+
+fn save_img(data: &String) {
+    let mut f = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("./img.ppm")
+        .expect("Unable to open file");
+    f.write_all(data.as_bytes()).expect("Unable to write data");
+}
+
+fn random_color() -> String {
+    let mut rng = rand::thread_rng();
+
+    let r = rng.gen_range(0..=255);
+    let g = rng.gen_range(0..=255);
+    let b = rng.gen_range(0..=255);
+
+    format!("{} {} {} ", r, g, b)
+}
+
+fn random_img(width: i32, height: i32) -> String {
+    let mut data = format!("P3\n{} {}\n255", width, height);
+
+    for _ in 0..=height {
+        let mut row = String::from("\n");
+        for _ in 0..=width {
+            row.push_str(&random_color());
+        }
+        data.push_str(&row);
+    }
+
+    data
+}
+
+fn main() {
+    let img = random_img(500, 500);
+    save_img(&img);
+
+    eprintln!("test");
+}
